4cf20e46623379cc7e4b8e72e0167a459bb69f36
Ahmed Elbagory <ahmed.elbagory1@gmail.com>
2024-07-18 09:09:05 -0400

    4 deployment tests passing

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,49 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
-  it("saves the addresses", async () => {
+  let buyer, seller, inspector, lender;
+  let realEstate;
+
+  beforeEach(async () => {
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+    //deploy Real Estate
     const RealEstate = await ethers.getContractFactory("RealEstate");
     realEstate = await RealEstate.deploy();
 
-    console.log(realEstate.address);
+    //Mint
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.png"
+      );
+    await transaction.wait();
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
+  });
+
+  describe("Deployment", () => {
+    it("Returns NFT address", async () => {
+      const result = await escrow.nftAddress();
+      expect(result).to.be.equal(realEstate.address);
+    });
+    it("Returns sellers", async () => {
+      const result = await escrow.seller();
+      expect(result).to.be.equal(seller.address);
+    });
+    it("Returns inspector", async () => {
+      const result = await escrow.inspector();
+      expect(result).to.be.equal(inspector.address);
+    });
+    it("Returns lender", async () => {
+      const result = await escrow.lender();
+      expect(result).to.be.equal(lender.address);
+    });
   });
 });
